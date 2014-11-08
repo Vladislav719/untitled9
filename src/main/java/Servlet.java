2cf78b71f57b8b467542d4bd4dd64c3f14d076f2
Vladislav Baimurzin <baimurzin.719@gmail.com>
2014-11-08 15:38:09 +0400

    Initial commit

---
+++ src/main/java/Servlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by vladislav on 08.11.14.
+ */
+@WebServlet(urlPatterns = "/index")
+public class Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("index.jsp");
+        dispatcher.forward(req,resp);
+
+    }
+}
